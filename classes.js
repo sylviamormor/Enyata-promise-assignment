c32b18a4ae7d573ee8b2520712bba5619101e56a
Sylvia Mormor <sylvia.mormor@gmail.com>
2023-07-24 23:51:24 +0000

    current file

---
+++ classes.js
@@ -0,0 +1,25 @@
+class student {
+    constructor (name, age, score, courses){
+        this.name = name;
+        this.age = age;
+        this.score = score;
+        this.courses = courses;
+    }
+    favName(){
+     return this.name
+    }
+    favName(){
+        return this.age
+    }
+    favName(){
+         this.score =+ 1
+         return this.score
+    }
+    favName(){
+        return this.courses.push(courses)
+
+    }
+   }
+   
+const studentOne = new student('allison', 17, 30, ['math', ' science'])
+console.log(studentOne)
